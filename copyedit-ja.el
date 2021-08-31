302a0894d69dd3f6b686d5aa04b221bf32907a59
Hisashi Morita <hisashim@workbook.org>
2021-08-31 13:01:48 +0900

    Update comments and README

--- copyedit-ja.el
+++ copyedit-ja.el
@@ -1,17 +1,30 @@
-;; copyedit-ja.el --- Copyediting tool for Japanese text
+;;; copyedit-ja.el --- Copyediting tool for Japanese text
 ;; -*- encoding: utf-8; lexical-binding: t -*-
 ;;
-;;; Commentary:
-;;
-;; Copyedit-ja: Copyediting tool for Japanese text
 ;; Copyright (c) 2008-2021 Hisashi Morita
 ;; License: Public Domain
 ;;
-;; Usage Example:
+;;; Commentary:
+;;
+;; Synopsis:
 ;;   1. Check possible typos.
-;;     M-x copyedit-ja-check-typo-particles
+;;      M-x copyedit-ja-check-typo-particles
 ;;   2. Edit them.
-;;     M-x copyedit-ja-normalize-typo-particles
+;;      M-x copyedit-ja-normalize-typo-particles
+;;   (See color-moccur for detail, since copyedit-ja is basically just a
+;;   wrapper around color-moccur with preset regexp patterns.)
+;;
+;; Features:
+;;   Copyedit-ja helps checking and resolving issues with:
+;;     * Possible typos (successive ga, no, ni, wo, etc.)
+;;     * Character widths (numerals, parentheses, etc.)
+;;     * Matching of opening and closing parentheses
+;;     * Style (direct style (da-dearu) and distal style (desu-masu))
+;;     * Commas and periods (ten-maru and kan-piri)
+;;     * Uses of prolonged sound marks (onbiki)
+;;     * Space characters between latin text and Japanese text,
+;;   etc.
+;;   Try M-x copyedit-ja- TAB to list available commands.
 ;;
 ;; Requirements:
 ;;   * color-moccur: multi-buffer occur (grep) mode
@@ -23,30 +36,37 @@
 ;;   * MeCab: Yet another Japanese morphological analyzer (optional)
 ;;     - https://github.com/taku910/mecab
 ;;
-;; How to install:
+;; Installation:
 ;;   1. Install requirements.
-;;     - Install color-moccur using Emacs package manager.
-;;       (See https://melpa.org for instructions.)
-;;     - Install MeCab and its UTF-8 dictionary, e.g.
-;;       $ sudo apt install mecab mecab-ipadic-utf8
+;;      - Install color-moccur using Emacs package manager.
+;;        (See https://melpa.org for instructions.)
+;;      - Install MeCab and its UTF-8 dictionary, e.g.
+;;        $ sudo apt install mecab mecab-ipadic-utf8
 ;;   2. Install copyedit-ja.
-;;     - Put copyedit-ja.el somewhere in your load-path, e.g.
-;;       $ cp copyedit-ja.el ~/.emacs.d/lisp/
-;;   3. Configure copyedit-ja.
-;;     - If you prefer, load copyedit-ja on startup, e.g.
-;;       $ echo "(require 'copyedit-ja)" >> ~/emacs.d/init.el
+;;      - Put copyedit-ja.el somewhere in your load-path, e.g.
+;;        $ cp copyedit-ja.el ~/.emacs.d/lisp/
+;;      - If you prefer, load copyedit-ja on startup, e.g.
+;;        $ echo "(require 'copyedit-ja)" >> ~/emacs.d/init.el
+;;   3. Configure copyedit-ja (optional).
+;;      - Tweak dictionaries to fit your needs in your init file, e.g.
+;;        (setq copyedit-ja--dict-user-specified-keywords
+;;              (append copyedit-ja--dict-user-specified-keywords
+;;                      '(("TCP-IP". "TCP/IP"))))
 ;;
 ;; References:
+;;   Copyedit-ja is inspired and heavily influenced by respectable forerunners'
+;;   works.
 ;;   * [KSU1989]
 ;;     Masanori Kurata, Akira Suganuma, Kazuo Ushijima,
 ;;     "Development of a System of Writing Tools for Japanese Documents on
 ;;     a Personal Computer.",
 ;;     Computer Software, Vol. 6, No. 4 (1989), pp. 55-67.
-;;     - https://ci.nii.ac.jp/naid/110003743558
-;;     - https://www.jstage.jst.go.jp/article/jssst/6/4/6_4_373/_article/-char/ja
+;;       - https://ci.nii.ac.jp/naid/110003743558
+;;       - https://www.jstage.jst.go.jp/article/jssst/6/4/6_4_373/_article/-char/ja
 ;;   * [Urayama1998]
 ;;     Takeshi Urayama, Denshi Henshu no Susume: sed no Katsuyou
 ;;     (E-editing using Sed), 1998, Doseisha.
+;;       - https://ci.nii.ac.jp/ncid/BA38403160
 
 (require 'cl-lib)
 (require 'color-moccur)
